d253763d10f242794eaa6757977fb403c398cd00
Adebayo <54339202+Braggedtooth@users.noreply.github.com>
2024-03-28 15:55:26 +0100

    Add @vercel/analytics and @vercel/speed-insights packages

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import "./index.css";
 import { Dashboard } from "@/components/layout";
 import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,5 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Dashboard />
     </QueryClientProvider>
     <Toaster />
+    <SpeedInsights />
+    <Analytics />
   </React.StrictMode>,
 );
